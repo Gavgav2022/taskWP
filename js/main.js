bb983c1ac05c1305cb9cd93f682fc6780f88c479
Sergey Gavrilenko <svgavrilenko1994@gmail.com>
2024-03-29 14:52:06 +0200

    first c

---
+++ js/main.js
@@ -0,0 +1,20 @@
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    loop: true,
+    slidesPerView: 2,
+    breakpoints: {
+        768: {
+          slidesPerView: 4,
+        },
+      },
+});
+
+let burger = document.querySelector('.burger');
+let header = document.querySelector('.header');
+let menu = document.querySelector('.menu__list');
+
+burger.addEventListener('click', (e) => {
+    burger.classList.toggle('active');
+    header.classList.toggle('active');
+    menu.classList.toggle('active');
+})
